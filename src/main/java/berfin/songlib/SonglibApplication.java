b68384895244e9f4f745b36667d3031271651dca
Berfin Dicle Kılınç <berfindiclekilinc@gmail.com>
2024-06-13 13:33:32 +0300

    first commit

---
+++ src/main/java/berfin/songlib/SonglibApplication.java
@@ -0,0 +1,13 @@
+package berfin.songlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonglibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonglibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/berfin/songlib/SonglibApplicationTests.java
@@ -0,0 +1,13 @@
+package berfin.songlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonglibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
